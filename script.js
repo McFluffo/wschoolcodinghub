12e49ed5abe50c4b28a9862cad521d6c18ec101a
McFluffo <125170563+McFluffo@users.noreply.github.com>
2023-12-13 14:59:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function checkPassword() {
     var enteredPassword = document.getElementById("password").value;
-    // Replace 'yourpassword' with your chosen password
-    if (enteredPassword === "yourpassword") {
-        // Redirect to the desired page
-        window.location.href = "granted_access_page.html";
-    } else {
-        // Show an error message
-        alert("Incorrect password. Access denied!");
+    // List of correct passwords
+    var correctPasswords = ["ArizonaRoster$", "StinkyMonkey$"];
+
+    var passwordMatch = false;
+
+    // Check if the entered password matches any of the correct passwords
+    for (var i = 0; i < correctPasswords.length; i++) {
+        if (enteredPassword === correctPasswords[i]) {
+            passwordMatch = true;
+            // Redirect to the 'activitylist' URL upon correct password entry
+            window.location.href = "activitylist";
+            break;
+        }
+    }
+
+    if (!passwordMatch) {
+        // Show an error message for incorrect password
+        alert("Invalid Code");
     }
 }
